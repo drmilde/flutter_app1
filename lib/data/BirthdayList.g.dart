a52823ce5cae2cc2e390468cbf35d33ea61a9d95
milde <milde@hs-fulda.de>
2019-08-22 19:08:12 +0200

    generated json code

---
+++ lib/data/BirthdayList.g.dart
@@ -0,0 +1,14 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'BirthdayList.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+BirthdayList _$BirthdayListFromJson(Map<String, dynamic> json) {
+  return BirthdayList();
+}
+
+Map<String, dynamic> _$BirthdayListToJson(BirthdayList instance) =>
+    <String, dynamic>{};
